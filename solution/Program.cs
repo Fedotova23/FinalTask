0a29d6c4e37c7bf7958052a4091f61a8ceb2a203
Liza <lizafedotova171197@gmail.com>
2023-02-24 18:38:01 +0300

    add_method_GetNumberOfShortElements

---
+++ solution/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+string[] str = {"некоторый", "массив", "слов",  "где", "нужно", "выбрать", "слова", ",", "состоящие", "из",  "1-3",  "символов"};
+
+System.Console.WriteLine($"кол-во слов, состоящих из 1-3 символов = {GetNumberOfShortElements(str)}");
+
+
+int GetNumberOfShortElements(string[] str)
+{
+    int number = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if(str[i].Length <= 3)
+        {
+            number++;
+        }
+    }
+    return number;
+}
+
+
