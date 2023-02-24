99bc1dbb4adc4ff34c2e2511763ad09ebe9379f6
Liza <lizafedotova171197@gmail.com>
2023-02-24 19:17:28 +0300

    AddFullSolution

--- solution/Program.cs
+++ solution/Program.cs
@@ -1,8 +1,9 @@
 ﻿
 
 string[] str = {"некоторый", "массив", "слов",  "где", "нужно", "выбрать", "слова", ",", "состоящие", "из",  "1-3",  "символов"};
-
-System.Console.WriteLine($"кол-во слов, состоящих из 1-3 символов = {GetNumberOfShortElements(str)}");
+int numOfShortWord = GetNumberOfShortElements(str);
+System.Console.WriteLine($"кол-во слов, состоящих из 1-3 символов = {numOfShortWord}");
+PrintStr(GetNewStringWithShortWords(str, numOfShortWord));
 
 
 int GetNumberOfShortElements(string[] str)
@@ -17,5 +18,31 @@ int GetNumberOfShortElements(string[] str)
     }
     return number;
 }
+
+string[] GetNewStringWithShortWords ( string[] str, int size)
+{
+    string[] shortStr = new string[size];
+
+    int j = 0;
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3)
+        {
+            shortStr[j++] = str [i];
+        }
+    }
+    return shortStr;
+}
+
+void PrintStr (string[] str)
+{
+    for (int i = 0; i < numOfShortWord; i++)
+    {
+    System.Console.Write($"{str[i]} ");  
+    }
+}
+
+
 
 
